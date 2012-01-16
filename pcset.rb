902965c3a409e75c217246f988abea200c03d82b
Beau Sievers <beau@beausievers.com>
2012-01-15 19:01:24 -0500

    Converted everything to snake_case; other minor style changes

--- pcset.rb
+++ pcset.rb
@@ -24,25 +24,25 @@ def append_all(lists, element)
   lists.map { |l| l << element }
 end
 
-def arrayToBinary(array)
+def array_to_binary(array)
   array.inject(0) {|sum, n| sum + 2**n}
 end
 
 # the following method is horrifically inelegant 
 # but avoids monkey-patching.
-# TODO: do this right, incl. good error checking
+# TODO: do this right, incl. error checking
 def pearsons(x, y)
   if !x.is_a?(Array) || !y.is_a?(Array) then raise StandardError, "x and y must be arrays", caller end
   if x.size != y.size then raise StandardError, "x and y must be same size", caller end
-  sumX = x.inject(0) {|sum, n| sum + n}
-  sumY = y.inject(0) {|sum, n| sum + n}
-  sumSquareX = x.inject(0) {|sum, n| sum + n * n}
-  sumSquareY = y.inject(0) {|sum, n| sum + n * n}
+  sum_x = x.inject(0) {|sum, n| sum + n}
+  sum_y = y.inject(0) {|sum, n| sum + n}
+  sum_square_x = x.inject(0) {|sum, n| sum + n * n}
+  sum_square_y = y.inject(0) {|sum, n| sum + n * n}
   xy = []
   x.zip(y) {|a, b| xy.push(a * b)}
-  sumXY = xy.inject(0) {|sum, n| sum + n}
-  num = sumXY - ((sumX * sumY)/x.size)
-  den = Math.sqrt((sumSquareX - ((sumX*sumX)/x.size)) * (sumSquareY - ((sumY*sumY)/x.size)))
+  sum_xy = xy.inject(0) {|sum, n| sum + n}
+  num = sum_xy - ((sum_x * sum_y)/x.size)
+  den = Math.sqrt((sum_square_x - ((sum_x*sum_x)/x.size)) * (sum_square_y - ((sum_y*sum_y)/x.size)))
   (num/den)
 end
 
@@ -50,7 +50,7 @@ class PCSet
   include Comparable
   attr_reader :pitches, :base, :input
   
-  def initialize(pcarray, base=12)
+  def initialize(pcarray, base = 12)
     if pcarray.instance_of?(Array) && pcarray.all?{|pc| pc.instance_of?(Fixnum)}
       @base, @input = base, pcarray
       @pitches = pcarray.map{ |x| x % @base }.uniq
@@ -59,7 +59,7 @@ class PCSet
     end
   end
   
-  def PCSet.newFromString(pcstring, base=12)
+  def PCSet.new_from_string(pcstring, base = 12)
     if base > 36 then raise StandardError, "Use PCSet.new to create pcsets with a base larger than 36", caller end
     pcarray = []
     pcstring.downcase.split(//).each do |c|
@@ -94,11 +94,11 @@ class PCSet
     @pitches.length
   end
   
-  def invert(axis=0)
+  def invert(axis = 0)
     PCSet.new @pitches.map {|x| (axis-x) % @base}
   end
   
-  def invert!(axis=0)
+  def invert!(axis = 0)
     @pitches.map! {|x| (axis-x) % @base}
   end
     
@@ -130,72 +130,72 @@ class PCSet
     (0..(@base-1)).to_a.map{|x| @pitches.map {|y| (y + x) % @base}}.sort.map {|x| PCSet.new x}
   end
   
-  def transpositionsAndInversions(axis=0)
+  def transpositions_and_inversions(axis = 0)
     transpositions + invert(axis).transpositions
   end
   
   #
   # Normal form after Straus. Morris and AthenaCL do this differently.
   #
-  def normalForm
+  def normal_form
     tempar = @pitches.sort
     arar = []                 # [[1,4,7,8,10],[4,7,8,10,1], etc.] get each cyclic variation
     tempar.each {arar.push PCSet.new(tempar.unshift(tempar.pop))}
-    mostLeftCompact(arar)
+    most_left_compact(arar)
   end
   
-  def normalForm!
-    @pitches = normalForm.pitches
+  def normal_form!
+    @pitches = normal_form.pitches
   end
   
-  def isNormalForm?
-    self.pitches == self.normalForm.pitches
+  def is_normal_form?
+    self.pitches == self.normal_form.pitches
   end
   
-  def setClass
-    transpositionsAndInversions.map{|pcs| pcs.normalForm}.sort
+  def set_class
+    transpositions_and_inversions.map{|pcs| pcs.normal_form}.sort
   end
   
   def prime
-    mostLeftCompact([normalForm.zero, invert.normalForm.zero])
+    most_left_compact([normal_form.zero, invert.normal_form.zero])
   end
   
   def prime!
     self.pitches = self.prime.pitches
   end
   
-  def isPrime?
+  def is_prime?
     self.pitches == self.prime.pitches
   end
   
   def complement
-    newPitches = []
+    new_pitches = []
     @base.times do |p|
       if !@pitches.include? p then
-        newPitches.push p
+        new_pitches.push p
       end
     end
-    PCSet.new newPitches
+    PCSet.new new_pitches
   end
   
-  def fullIntervalVector
+  def full_interval_vector
     pairs = choose(@pitches, 2)                       # choose every pc pair
     intervals = pairs.map {|x| (x[1] - x[0]) % @base} # calculate every interval
-    iVector = Array.new(@base-1).fill(0)
-    intervals.each {|x| iVector[x-1] += 1}            # count the intervals
-    iVector
+    i_vector = Array.new(@base-1).fill(0)
+    intervals.each {|x| i_vector[x-1] += 1}            # count the intervals
+    i_vector
   end
   
-  def intervalVector
-    iVector = fullIntervalVector
-    (0..((@base-1)/2)-1).each {|x| iVector[x] += iVector.pop}
-    iVector
+  def interval_vector
+    i_vector = full_interval_vector
+    (0..((@base-1)/2)-1).each {|x| i_vector[x] += i_vector.pop}
+    i_vector
   end
   
   #
   # Morris's invariance vector
   #
-  def invarianceVector(m = 5)
+  def invariance_vector(m = 5)
     t = transpositions.map!{|pcs| self & pcs}
     ti = invert.transpositions.map!{|pcs| self & pcs}    
     tm = multiply(m).transpositions.map!{|pcs| self & pcs}   
@@ -216,11 +216,11 @@ class PCSet
     if @base != 12 then raise StandardError, "PCSet.huron only makes sense for mod 12 pcsets", caller end
       
     #              m2/M7   M2/m7  m3/M6  M3/m6  P4/P5   A4/d5
-    huronTable = [-1.428, -0.582, 0.594, 0.386, 1.240, -0.453]
-    intervalConsonance = []
-    intervalVector.zip(huronTable) {|x, y| intervalConsonance.push(x * y) }
-    aggregateDyadicConsonance = intervalConsonance.inject {|sum, n| sum + n}
-    [aggregateDyadicConsonance, pearsons(intervalVector, huronTable)]
+    huron_table = [-1.428, -0.582, 0.594, 0.386, 1.240, -0.453]
+    interval_consonance = []
+    interval_vector.zip(huron_table) {|x, y| interval_consonance.push(x * y) }
+    aggregate_dyadic_consonance = interval_consonance.inject {|sum, n| sum + n}
+    [aggregate_dyadic_consonance, pearsons(interval_vector, huron_table)]
   end
 
   #
@@ -229,7 +229,7 @@ class PCSet
   # Balzano. "The Pitch Set as a Level of Description for Studying Musical
   # Pitch Perception" in Music, Mind, and Brain ed. Clynes. Plenum Press. 1982.
   #
-  def vectorOfRelations
+  def vector_of_relations
     (0..length-1).to_a.map do |i|
       (0..length-1).to_a.map do |j|
         (@pitches[(i + j) % length] - @pitches[i]) % @base
@@ -240,8 +240,8 @@ class PCSet
   #
   # Checks if the set satisfies Balzano's uniqueness.
   #
-  def isUnique?
-    vectorOfRelations.uniq.size == vectorOfRelations.size
+  def is_unique?
+    vector_of_relations.uniq.size == vector_of_relations.size
   end
   
   #
@@ -251,51 +251,51 @@ class PCSet
   # Where j and k are scalestep-counting indices.
   # And unless v[i][j] == 6 (a tritone), in which case the strict inequality is relaxed.
   #
-  def isCoherent?
-    v = vectorOfRelations
-    truthArray = []
-    allPairIndices = choose((0..length-1).to_a, 2)
-    allPairIndices.each do |i, i1|
-      allPairIndices.each do |j, k|
+  def is_coherent?
+    v = vector_of_relations
+    truth_array = []
+    all_pair_indices = choose((0..length-1).to_a, 2)
+    all_pair_indices.each do |i, i1|
+      all_pair_indices.each do |j, k|
         if v[i][j] == 6
-          truthArray.push(v[i][j] <= v[i1][k])
+          truth_array.push(v[i][j] <= v[i1][k])
         else
-          truthArray.push(v[i][j] < v[i1][k])    
+          truth_array.push(v[i][j] < v[i1][k])    
         end
         if v[i1][j] == 6
-          truthArray.push(v[i1][j] <= v[i][k])
+          truth_array.push(v[i1][j] <= v[i][k])
         else
-          truthArray.push(v[i1][j] < v[i][k])
+          truth_array.push(v[i1][j] < v[i][k])
         end
       end
     end
-    !truthArray.include?(false)
+    !truth_array.include?(false)
   end
   
   #
   # Strict Balzano coherence, no inequality relaxation for tritones.
   #
-  def isStrictlyCoherent?
-    v = vectorOfRelations
-    truthArray = []
-    allPairIndices = choose((0..length-1).to_a, 2)
-    allPairIndices.each do |i, i1|
-      allPairIndices.each do |j, k|
-        truthArray.push(v[i][j] < v[i1][k])
-        truthArray.push(v[i1][j] < v[i][k])
+  def is_strictly_coherent?
+    v = vector_of_relations
+    truth_array = []
+    all_pair_indices = choose((0..length-1).to_a, 2)
+    all_pair_indices.each do |i, i1|
+      all_pair_indices.each do |j, k|
+        truth_array.push(v[i][j] < v[i1][k])
+        truth_array.push(v[i1][j] < v[i][k])
       end
     end
-    !truthArray.include?(false)
+    !truth_array.include?(false)
   end
   
-  def notes(middleC=0)
+  def notes(middle_c = 0)
     noteArray = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B']
     if @base != 12 then raise StandardError, "PCSet.notes only makes sense for mod 12 pcsets", caller end
-    outString = String.new
-    transpose(-middleC).pitches.each do |p|
-      outString += noteArray[p] + ", "
+    out_string = String.new
+    transpose(-middle_c).pitches.each do |p|
+      out_string += noteArray[p] + ", "
     end
-    outString.chop.chop
+    out_string.chop.chop
   end
 
   def info
@@ -303,15 +303,15 @@ class PCSet
     print "raw input: #{@input.inspect}\n"
     print "pitch set: #{@pitches.inspect}\n"
     print "notes: #{notes}\n"
-    print "normal: #{normalForm.inspect}\n"
+    print "normal: #{normal_form.inspect}\n"
     print "prime: #{prime.inspect}\n"
-    print "interval vector: #{intervalVector.inspect}\n"
-    print "invariance vector: #{invarianceVector.inspect}\n"
+    print "interval vector: #{interval_vector.inspect}\n"
+    print "invariance vector: #{invariance_vector.inspect}\n"
     print "huron ADC: #{huron[0]}  pearsons: #{huron[1]}\n"
     print "balzano coherence: "
-    if isStrictlyCoherent?
+    if is_strictly_coherent?
       print "strictly coherent\n"
-    elsif isCoherent?
+    elsif is_coherent?
       print "coherent\n"
     else
       print "false\n"
@@ -336,15 +336,15 @@ class PCSet
   #           2^n:  BA9876543210
   # The smallest binary number is the most left-compact.
   #
-  def mostLeftCompact(pcSetArray)
-    if !pcSetArray.all? {|pcs| pcs.length == pcSetArray[0].length}
-      raise ArgumentError, "PCSet.mostLeftCompact: All PCSets must be of same cardinality", caller
+  def most_left_compact(pcset_array)
+    if !pcset_array.all? {|pcs| pcs.length == pcset_array[0].length}
+      raise ArgumentError, "PCSet.most_left_compact: All PCSets must be of same cardinality", caller
     end
-    zeroedPitchArrays = pcSetArray.map {|pcs| pcs.zero.pitches}
-    binaries = zeroedPitchArrays.map {|array| arrayToBinary(array)}
+    zeroed_pitch_arrays = pcset_array.map {|pcs| pcs.zero.pitches}
+    binaries = zeroed_pitch_arrays.map {|array| array_to_binary(array)}
     winners = []
     binaries.each_with_index do |num, i|
-      if num == binaries.min then winners.push(pcSetArray[i]) end
+      if num == binaries.min then winners.push(pcset_array[i]) end
     end
     winners.sort[0]
   end
--- pcset_test.rb
+++ pcset_test.rb
@@ -1,4 +1,4 @@
-require 'pcset.rb'
+require './pcset.rb'
 require 'test/unit'
 
 #
@@ -14,7 +14,7 @@ class PCSetTest < Test::Unit::TestCase
     assert_raise(ArgumentError) {PCSet.new "string"}
     assert_raise(ArgumentError) {PCSet.new [1, 2, 3.6, 4]}
     assert_equal([0, 1, 2, 9], PCSet.new([0, 1, 2, 33, 13]).pitches)
-    assert_equal([3, 2, 1, 11, 10, 0], PCSet.newFromString('321bac').pitches)
+    assert_equal([3, 2, 1, 11, 10, 0], PCSet.new_from_string('321bac').pitches)
     assert_equal([0,2,4,5,7,11,9], PCSet.new([12,2,4,5,7,11,9]).pitches)
     assert_nothing_raised() {PCSet.new []}
   end
@@ -38,18 +38,18 @@ class PCSetTest < Test::Unit::TestCase
   # 0,1,3,5,6,7,9,10,11   5,6,7,9,10,11,0,1,3    0,1,2,3,4,6,7,8,10    9-8
   #
   
-  def test_normalForm
+  def test_normal_form
     testPC = PCSet.new [0,4,8,9,11]
-    assert_kind_of(PCSet, testPC.normalForm)
-    assert_equal([8,9,11,0,4], testPC.normalForm.pitches)
-    assert_equal([10,1,4,6], PCSet.new([1,6,4,10]).normalForm.pitches)
-    assert_equal([2,4,8,10], PCSet.new([10,8,4,2]).normalForm.pitches)
-    assert_equal([7,8,11,0,2,4], PCSet.new([0,2,4,7,8,11]).normalForm.pitches)
-    assert_equal([11,0,1,2,4,5,7], PCSet.new([0,1,2,4,5,7,11]).normalForm.pitches)
-    assert_equal([5,6,7,9,10,11,0,1,3], PCSet.new([0,1,3,5,6,7,9,10,11]).normalForm.pitches)  
+    assert_kind_of(PCSet, testPC.normal_form)
+    assert_equal([8,9,11,0,4], testPC.normal_form.pitches)
+    assert_equal([10,1,4,6], PCSet.new([1,6,4,10]).normal_form.pitches)
+    assert_equal([2,4,8,10], PCSet.new([10,8,4,2]).normal_form.pitches)
+    assert_equal([7,8,11,0,2,4], PCSet.new([0,2,4,7,8,11]).normal_form.pitches)
+    assert_equal([11,0,1,2,4,5,7], PCSet.new([0,1,2,4,5,7,11]).normal_form.pitches)
+    assert_equal([5,6,7,9,10,11,0,1,3], PCSet.new([0,1,3,5,6,7,9,10,11]).normal_form.pitches)  
   end
   
-  def test_primeForm
+  def test_prime_form
     assert_equal([0,1,2,6], PCSet.new([5,6,1,7]).prime.pitches)
     assert_equal([0,1,4], PCSet.new([2,5,6]).prime.pitches)
     assert_equal([0,1,4,5,7,9], PCSet.new([0,2,4,7,8,11]).prime.pitches)
@@ -57,7 +57,7 @@ class PCSetTest < Test::Unit::TestCase
     assert_equal([0,1,2,3,4,6,7,8,10], PCSet.new([0,1,3,5,6,7,9,10,11]).prime.pitches)
   end
   
-  def test_setClass
+  def test_set_class
     testPcs = PCSet.new([2,5,6])
     testPrime = testPcs.prime
     assert_equal([
@@ -65,14 +65,14 @@ class PCSetTest < Test::Unit::TestCase
         [8,11,0],[9,0,1], [10,1,2],[11,2,3],[0,3,4],  [1,4,5],
         [6,7,10],[7,8,11],[8,9,0], [9,10,1],[10,11,2],[11,0,3],
         [0,1,4], [1,2,5], [2,3,6], [3,4,7], [4,5,8],  [5,6,9]
-      ].sort, PCSet.new([2,5,6]).setClass.map{|x| x.pitches})
-    assert_equal(testPcs.setClass.map{|x| x.pitches}, testPrime.setClass.map{|x| x.pitches})
+      ].sort, PCSet.new([2,5,6]).set_class.map{|x| x.pitches})
+    assert_equal(testPcs.set_class.map{|x| x.pitches}, testPrime.set_class.map{|x| x.pitches})
   end
   
-  def test_intervalVector
-    assert_equal([2,1,2,1,0,0], PCSet.new([0,1,3,4]).intervalVector)
-    assert_equal([2,5,4,3,6,1], PCSet.new([0,1,3,5,6,8,10]).intervalVector)
-    assert_equal([0,6,0,6,0,3], PCSet.new([0,2,4,6,8,10]).intervalVector)
+  def test_interval_vector
+    assert_equal([2,1,2,1,0,0], PCSet.new([0,1,3,4]).interval_vector)
+    assert_equal([2,5,4,3,6,1], PCSet.new([0,1,3,5,6,8,10]).interval_vector)
+    assert_equal([0,6,0,6,0,3], PCSet.new([0,2,4,6,8,10]).interval_vector)
   end
   
   def test_complement
@@ -86,13 +86,13 @@ class PCSetTest < Test::Unit::TestCase
   # Morris. Class Notes for Atonal Music Theory
   # Lebanon, NH. Frog Peak Music, 1991.
   #
-  def test_invarianceVector
-    assert_equal([1,0,0,0,5,6,5,5],PCSet.new([0,2,5]).invarianceVector)
-    assert_equal([2,2,2,2,6,6,6,6],PCSet.new([0,1,6,7]).invarianceVector)
-    assert_equal([6,6,6,6,6,6,6,6],PCSet.new([0,2,4,6,8,10]).invarianceVector)
-    assert_equal([1,0,0,0,0,0,0,0],PCSet.new([0,1,2,3,4,5,8]).invarianceVector)
-    assert_equal([1,0,0,1,0,0,0,0],PCSet.new([0,1,2,3,5,6,8]).invarianceVector)
-    assert_equal([12,12,12,12,0,0,0,0],PCSet.new([0,1,2,3,4,5,6,7,8,9,10,11]).invarianceVector)
+  def test_invariance_vector
+    assert_equal([1,0,0,0,5,6,5,5],PCSet.new([0,2,5]).invariance_vector)
+    assert_equal([2,2,2,2,6,6,6,6],PCSet.new([0,1,6,7]).invariance_vector)
+    assert_equal([6,6,6,6,6,6,6,6],PCSet.new([0,2,4,6,8,10]).invariance_vector)
+    assert_equal([1,0,0,0,0,0,0,0],PCSet.new([0,1,2,3,4,5,8]).invariance_vector)
+    assert_equal([1,0,0,1,0,0,0,0],PCSet.new([0,1,2,3,5,6,8]).invariance_vector)
+    assert_equal([12,12,12,12,0,0,0,0],PCSet.new([0,1,2,3,4,5,6,7,8,9,10,11]).invariance_vector)
   end
   
   #
